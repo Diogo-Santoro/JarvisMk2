c9366dae3e152520fdea11223a6dc13eee07d4e8
Dias-Synthesis <diasg.contato@gmail.com>
2023-09-09 12:51:14 -0300

    Corrections & Commented code - Teste 1.0.1

--- src/test.py-ino/arduino_control/arduino_control.ino
+++ src/test.py-ino/arduino_control/arduino_control.ino
@@ -1,66 +1,78 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial bluetoothSerial(10, 11);
+SoftwareSerial bluetoothSerial(10, 11); // Pinos 10 (RX) e 11 (TX) para a comunicação Bluetooth.
 
-int pump1Pin = 2;
-int pump2Pin = 3;
-int pump3Pin = 4;
-int pump4Pin = 5;
+int pump1Pin = 2; // Define o pino digital 2 como o pino de controle da bomba 1.
+int pump2Pin = 3; // Define o pino digital 3 como o pino de controle da bomba 2.
+int pump3Pin = 4; // Define o pino digital 4 como o pino de controle da bomba 3.
+int pump4Pin = 5; // Define o pino digital 5 como o pino de controle da bomba 4.
 
 void setup() {
-  Serial.begin(9600);
-  bluetoothSerial.begin(9600);
-  pinMode(pump1Pin, OUTPUT);
-  pinMode(pump2Pin, OUTPUT);
-  pinMode(pump3Pin, OUTPUT);
-  pinMode(pump4Pin, OUTPUT);
-  digitalWrite(pump1Pin, LOW);
-  digitalWrite(pump2Pin, LOW);
-  digitalWrite(pump3Pin, LOW);
-  digitalWrite(pump4Pin, LOW);
+  Serial.begin(9600);  // Inicializa a comunicação serial padrão a uma taxa de transmissão de 9600 baud.
+  bluetoothSerial.begin(9600);  // Inicializa a comunicação serial Bluetooth a 9600 baud.
+  pinMode(pump1Pin, OUTPUT);  // Configura o pino da bomba 1 como saída.
+  pinMode(pump2Pin, OUTPUT);  // Configura o pino da bomba 2 como saída.
+  pinMode(pump3Pin, OUTPUT);  // Configura o pino da bomba 3 como saída.
+  pinMode(pump4Pin, OUTPUT);  // Configura o pino da bomba 4 como saída.
+  digitalWrite(pump1Pin, LOW);  // Desliga a bomba 1 no início.
+  digitalWrite(pump2Pin, LOW);  // Desliga a bomba 2 no início.
+  digitalWrite(pump3Pin, LOW);  // Desliga a bomba 3 no início.
+  digitalWrite(pump4Pin, LOW);  // Desliga a bomba 4 no início.
 }
 
 void testPump1() {
   digitalWrite(pump1Pin, HIGH);
+  bluetoothSerial.println("Bomba 1 ligada");  // Envia uma mensagem via Bluetooth
+  Serial.println("Comando recebido: 1");  // Imprime um log serial
 }
 
 void testPump2() {
   digitalWrite(pump2Pin, HIGH);
+  bluetoothSerial.println("Bomba 2 ligada");  // Envia uma mensagem via Bluetooth
+  Serial.println("Comando recebido: 2");  // Imprime um log serial
 }
 
 void testPump3() {
   digitalWrite(pump3Pin, HIGH);
+  bluetoothSerial.println("Bomba 3 ligada");  // Envia uma mensagem via Bluetooth
+  Serial.println("Comando recebido: 3");  // Imprime um log serial
 }
 
 void testPump4() {
   digitalWrite(pump4Pin, HIGH);
+  bluetoothSerial.println("Bomba 4 ligada");  // Envia uma mensagem via Bluetooth
+  Serial.println("Comando recebido: 4");  // Imprime um log serial
 }
 
 void loop() {
-  if (bluetoothSerial.available() > 0) {
-    char command = bluetoothSerial.read();
-    switch (command) {
-      case '1':
-        testPump1();
-        break;
-      case '2':
-        testPump2();
-        break;
-      case '3':
-        testPump3();
-        break;
-      case '4':
-        testPump4();
-        break;
-      case '0':
-        digitalWrite(pump1Pin, LOW);
-        digitalWrite(pump2Pin, LOW);
-        digitalWrite(pump3Pin, LOW);
-        digitalWrite(pump4Pin, LOW);
-        break;
-      default:
-        break;
+  if (bluetoothSerial.available()) {  // Verifica se há dados disponíveis na comunicação Bluetooth.
+    char command = bluetoothSerial.read();  // Lê um caractere (comando) da comunicação Bluetooth.
+    
+    switch (command) {  // Inicia uma estrutura de controle de switch com base no comando lido.
+      case '1':  // Se o comando for '1'.
+        testPump1();  // Chama a função para ligar a bomba 1.
+        break;  // Sai do switch.
+      case '2':  // Se o comando for '2'.
+        testPump2();  // Chama a função para ligar a bomba 2.
+        break;  // Sai do switch.
+      case '3':  // Se o comando for '3'.
+        testPump3();  // Chama a função para ligar a bomba 3.
+        break;  // Sai do switch.
+      case '4':  // Se o comando for '4'.
+        testPump4();  // Chama a função para ligar a bomba 4.
+        break;  // Sai do switch.
+      case '0':  // Se o comando for '0'.
+        digitalWrite(pump1Pin, LOW);  // Desliga a bomba 1.
+        digitalWrite(pump2Pin, LOW);  // Desliga a bomba 2.
+        digitalWrite(pump3Pin, LOW);  // Desliga a bomba 3.
+        digitalWrite(pump4Pin, LOW);  // Desliga a bomba 4.
+        bluetoothSerial.println("Todas as bombas desligadas");  // Envia uma mensagem via Bluetooth
+        Serial.println("Comando recebido: 0");  // Imprime um log serial
+        break;  // Sai do switch.
+      default:  // Se o comando não for reconhecido.
+        Serial.println("Comando recebido inválido");  // Imprime um log serial
+        break;  // Sai do switch.
     }
   }
 }
--- src/test.py-ino/python_gui/gui.py
+++ src/test.py-ino/python_gui/gui.py
@@ -1,63 +1,82 @@
-import tkinter as tk
-import serial
-import time
+import tkinter as tk  # Importa a biblioteca Tkinter (para interface gráfica)
+#import bluetooth  # Importa a biblioteca Bluetooth (comentada porque não é usada no código atual)
 
-class Application(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.title("Controle de Bombas")
-        self.geometry("800x600")
-        self.create_widgets()
-        #self.arduino = serial.Serial('COM3', 9600)
+class Application(tk.Tk):  # Define uma classe chamada Application que herda de tk.Tk
+    def __init__(self):  # Define o construtor da classe
+        super().__init__()  # Chama o construtor da classe pai
+        self.title("Controle de Bombas")  # Define o título da janela
+        self.geometry("800x600")  # Define o tamanho da janela
+        self.create_widgets()  # Chama o método para criar widgets
 
-    def create_widgets(self):
-        test_menu_button = tk.Button(self, text="Test Menu", command=self.open_test_menu)
-        test_menu_button.pack(pady=100)
+        # Endereço MAC do módulo Bluetooth do Arduino (substitua pelo seu endereço)
+        #self.arduino_mac_address = '00:00:00:00:00:00'
 
-    def open_test_menu(self):
-        test_menu_window = tk.Toplevel(self)
-        test_menu_window.title("Test Menu")
+        self.feedback_label = tk.Label(self, text="", fg="black")  # Cria um rótulo para exibir feedback
+        self.feedback_label.pack()  # Adiciona o rótulo à janela
 
-        btn_testPump1 = tk.Button(test_menu_window, text="Test Pump 1", command=self.test_pump1)
-        btn_testPump2 = tk.Button(test_menu_window, text="Test Pump 2", command=self.test_pump2)
-        btn_testPump3 = tk.Button(test_menu_window, text="Test Pump 3", command=self.test_pump3)
-        btn_testPump4 = tk.Button(test_menu_window, text="Test Pump 4", command=self.test_pump4)
-        btn_allPumps = tk.Button(test_menu_window, text="All Pumps", command=self.test_all_pumps)
-        btn_emergencyStop = tk.Button(test_menu_window, text="Emergency Stop", command=self.emergency_stop)
-        btn_back = tk.Button(test_menu_window, text="Back", command=test_menu_window.destroy)
+    def create_widgets(self):  # Define um método para criar widgets
+        test_menu_button = tk.Button(self, text="Test Menu", command=self.open_test_menu)  # Cria um botão chamado "Test Menu"
+        test_menu_button.pack(pady=100)  # Adiciona o botão à janela
 
-        buttons = [btn_testPump1, btn_testPump2, btn_testPump3, btn_testPump4, btn_allPumps, btn_emergencyStop]
-        row = 0
-        for i in range(0, len(buttons), 2):
-            buttons[i].grid(row=row, column=0, padx=10, pady=10)
-            if i + 1 < len(buttons):
-                buttons[i + 1].grid(row=row, column=1, padx=10, pady=10)
-            row += 1
-        btn_back.grid(row=row, column=0, columnspan=2, padx=10, pady=10)
+    def open_test_menu(self):  # Define um método para abrir o menu de teste
+        test_menu_window = tk.Toplevel(self)  # Cria uma nova janela
+        test_menu_window.title("Test Menu")  # Define o título da nova janela
+        test_menu_window.geometry("600x400")  # Define o tamanho da nova janela
 
-    def test_pump1(self):
-        self.send_command('1')
+        buttons = []  # Inicializa uma lista vazia para armazenar botões
+        for pump_number in range(1, 5):  # Loop de 1 a 4
+            button_text = f"Test Pump {pump_number}"  # Define o texto do botão dinamicamente
+            buttons.append(tk.Button(test_menu_window, text=button_text, command=lambda num=pump_number: self.test_pump(num)))  # Cria botões dinamicamente
 
-    def test_pump2(self):
-        self.send_command('2')
+        for button in buttons:  # Loop pelos botões criados
+            button.pack(fill='y', expand=False, pady=5)  # Adiciona os botões à nova janela
 
-    def test_pump3(self):
-        self.send_command('3')
+        btn_emergency_stop = tk.Button(test_menu_window, text="Emergency Stop", command=self.emergency_stop)  # Cria um botão "Emergency Stop"
+        btn_emergency_stop.pack(fill='y', expand=False, pady=5)  # Adiciona o botão à nova janela
 
-    def test_pump4(self):
-        self.send_command('4')
+    def test_pump(self, pump_number):  # Define um método para testar a bomba
+        command = str(pump_number)  # Converte o número da bomba em uma string
+        response = self.send_command(command)  # Chama a função para enviar comandos ao Arduino
+        feedback_text = f"Comando enviado: {command}, Resposta: {response}"  # Cria um texto de feedback
+        if "Erro" in response:  # Verifica se a resposta contém "Erro"
+            self.update_feedback(feedback_text, "red")  # Atualiza o feedback em vermelho em caso de erro
+        else:
+            self.update_feedback(feedback_text, "green")  # Atualiza o feedback em verde em caso de sucesso
 
-    def test_all_pumps(self):
-        for pump_number in range(1, 5):
-            self.send_command(str(pump_number))
-            time.sleep(2)
+    def emergency_stop(self):  # Define um método para a parada de emergência
+        command = '0'  # Define o comando de parada de emergência
+        response = self.send_command(command)  # Chama a função para enviar comandos ao Arduino
+        feedback_text = f"Comando enviado: {command}, Resposta: {response}"  # Cria um texto de feedback
+        if "Erro" in response:  # Verifica se a resposta contém "Erro"
+            self.update_feedback(feedback_text, "red")  # Atualiza o feedback em vermelho em caso de erro
+        else:
+            self.update_feedback(feedback_text, "green")  # Atualiza o feedback em verde em caso de sucesso
 
-    def emergency_stop(self):
-        self.send_command('0')
+    def send_command(self, command):  # Define um método para enviar comandos
+        try:
+            # Conecta-se ao módulo Bluetooth do Arduino pelo endereço MAC
+            sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+            sock.connect((self.arduino_mac_address, 1))
 
-    def send_command(self, command):
-        self.arduino.write(command.encode())
+            #Envia o comando para o Arduino via Bluetooth
+            sock.send(command)
 
-if __name__ == "__main__":
-    app = Application()
-    app.mainloop()
+            # Recebe a resposta do Arduino
+            response = sock.recv(1024)
+            sock.close()
+
+            # Simulação de resposta (descomente as linhas acima para usar o Bluetooth)
+            response = "Comando recebido pelo Arduino."
+
+            return response
+        except Exception as e:
+            error_message = f"Erro ao enviar comando para Arduino: {str(e)}"
+            self.update_feedback(error_message, "red")  # Atualiza o feedback em vermelho em caso de erro
+            return error_message
+
+    def update_feedback(self, text, color):  # Define um método para atualizar o feedback
+        self.feedback_label.config(text=text, fg=color)  # Atualiza o rótulo de feedback com texto e cor
+
+if __name__ == "__main__":  # Verifica se o código está sendo executado como um programa principal
+    app = Application()  # Cria uma instância da classe Application
+    app.mainloop()  # Inicia o loop principal da interface gráfica
